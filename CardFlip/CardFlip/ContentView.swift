a333f832cf2ba7d6f7ccc0c11608105c77499e75
zfert514 <61804342+zfert514@users.noreply.github.com>
2021-02-25 14:08:57 -0500

    cards

---
+++ CardFlip/CardFlip/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  CardFlip
+//
+//  Created by Zachary Fertig on 2/25/21.
+//  Copyright © 2021 Zachary Fertig. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Image("2C")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Image("2D")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            HStack {
+                Image("2H")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Image("2S")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
